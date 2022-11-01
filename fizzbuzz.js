d3a93b229f098c565afc74fe70dc14c13d3b7a52
Hathaway-Shan <hathaway.shan@gmail.com>
2022-11-01 11:47:06 -0700

    solution now displays on the html page

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,25 +1,34 @@
 //the program writes the numbers 1 through 100
 function fizzBuzz(num) {
+  const display = document.querySelector(".display");
+  const ul = document.querySelector("ul");
   //guard for correct input type
   if (typeof num !== "number") {
     throw new Error("FizzBuzz input must be a number");
   }
   for (i = 1; i <= num; i++) {
-    // console.log("i is: ", i); //prints 'i is: 1-100
+    const li = document.createElement("li");
+
     //if a number is evenly divisible by both 3 and 5 instead of writing the number write the string 'fizzbuzz'
     if (i % 15 === 0) {
-      console.log("fizzbuzz");
+      // console.log("Fizzbuzz");
+      li.innerHTML = `i is ${i} FizzBuzz`;
     }
     //for each number that is evenly divisible by 3 instead of writing the number write the string 'fizz'
     else if (i % 3 === 0) {
-      console.log("fizz");
+      // console.log("Fizz");
+      li.innerHTML = `i is ${i} Fizz`;
     }
     //for each number that is evenly divisible by 5 instead of writing the number write the string 'buzz'
     else if (i % 5 === 0) {
-      console.log("buzz");
+      // console.log("Buzz");
+      li.innerHTML = `i is ${i} Buzz`;
     } else {
-      console.log(i);
+      // console.log(i);
+      li.innerHTML = `i is ${i}`;
     }
+    ul.appendChild(li);
   }
+  display.appendChild(ul);
 }
 fizzBuzz(100);
